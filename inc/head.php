8f898ab313d240efe8fdc62485e8de62261cc828
Fırat Işıldak <firatisildak1@gmail.com>
2023-06-02 19:01:26 +0300

    menu duzeltme

--- inc/head.php
+++ inc/head.php
@@ -52,28 +52,34 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <!--ms-auto bu kısım menüyü ortalıyor.-->
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <?php
-           $menuItems = array();
-              //Alt'daki kod parçası bizim menüdeki kayıtlı verilerimizi getiriyor ve bu kodları bir foreach yapısı ile menülerin kayıt olduğu nav bar kısmındaki ilgili koda getiriyor.
-              $result = $db->query("SELECT menuName, menuUrl, menuikon FROM menutable");
-              if ($result->rowCount() > 0) {
-                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {//fetch(PDO::FETCH_ASSOC) yöntemi, sonuç kümesinden bir satırı alır ve bu satırı bir dizi olarak döndürür.
-                  $menuItem = array("link" => $row["menuUrl"], "icon" => $row["menuikon"], "text" => $row["menuName"]);//Döngü gövdesi kısmını özelleştirebilirsiniz. Bu bölümde, $row dizisinin herhangi bir özelliğini kullanabilirsiniz. Örneğin, $row['column_name'] şeklinde bir sütun adını kullanarak belirli bir sütundaki verilere erişebilirsiniz.
-                  $menuItems[] = $menuItem; // Diziye her adımda bir öğe eklenir
-                }
-              } else {
-              echo "Veritabanında hiç veri bulunamadı.";
-              }
-              $db = null; // Veritabanı bağlantısını kapatma
-              //alt'taki kod bloğu bizim array yapısında kullandığımız foreach yapısı bizim her bir $menuItems her seferinde $item değişkenine atayıp veritabaındaki veri kadar işlem yapıyor.
-              foreach ($menuItems as $item) {
-                echo '<li class="nav-item d-flex">';
-                echo '<a class="nav-link active" aria-current="page" href="' . $item['link'] . '">';
-                echo '<i ' . $item['icon'] . '></i>' . $item['text'] . '</a>';
-                echo '<span class="brace">|</span>';
-                echo '</li>';
-              }
-            ?>
+          <li class="nav-item d-flex"> <!--buradaki d-flex yapısı oluşturulan <span class="brace">|</span> ayraçları iconların yanlarına almasını sağlıyor.-->
+            <!--alt satırdaki iconları Font Awesome kısmından aldım.-->
+            <a class="nav-link active" aria-current="page" href="index.php"><i class="fa-solid fa-house icon"></i>Anasayfa</a>
+            <span class="brace">|</span>
+          </li>
+          <li class="nav-item d-flex">
+            <a class="nav-link active" aria-current="page" href="index.php#aboutUs"><i class="fa-solid fa-info icon"></i>Hakkımızda</a>
+            <span class="brace">|</span>
+          </li>
+          <li class="nav-item d-flex">
+            <a class="nav-link active" aria-current="page" href="egitimler.php"><i class="fa-solid fa-graduation-cap icon"></i>Eğitimler</a>
+            <span class="brace">|</span>
+          </li>
+          <li class="nav-item d-flex">
+            <a class="nav-link active" aria-current="page" href="#"><i class="fa-solid fa-cart-shopping icon"></i>Sepet</a>
+            <span class="brace">|</span>
+          </li>
+          <li class="nav-item d-flex">
+            <a class="nav-link active" aria-current="page" href="index.php#opaqueRow"><i class="fa-solid fa-map-pin icon"></i>İletişim</a>
+            <span class="brace">|</span>
+          </li>
+          <li class="nav-item d-flex">
+            <a class="nav-link active" aria-current="page" href="index.php#educator"><i class="fa-solid fa-people-group icon"></i>Eğitmenler</a>
+            <span class="brace">|</span>
+          </li>
+          <li class="nav-item d-flex">
+            <a class="nav-link active" aria-current="page" href="login_index.php"><i class="fa-solid fa-right-to-bracket icon"></i>Giriş&Kayıt</a>
+          </li>
         </ul>
       </div>
     </div> 
